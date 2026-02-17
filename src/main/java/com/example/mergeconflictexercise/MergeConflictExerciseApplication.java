3bf694a92c1960ea6b9829d1e69e788e26b1c00f
Reza <remo0002@stud.ek.dk>
2026-02-17 10:39:39 +0100

    Initial commit

---
+++ src/main/java/com/example/mergeconflictexercise/MergeConflictExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mergeconflictexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeConflictExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MergeConflictExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mergeconflictexercise/MergeConflictExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mergeconflictexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeConflictExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
